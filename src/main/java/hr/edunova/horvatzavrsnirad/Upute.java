55b72e3079c6390ef3eacc95d69012659ab52814
Josip Horvat <josiph988@gmail.com>
2020-08-25 17:18:18 +0100

    Test1

--- src/main/java/hr/edunova/horvatzavrsnirad/Upute.java
+++ src/main/java/hr/edunova/horvatzavrsnirad/Upute.java
@@ -7,6 +7,10 @@ public class Upute {
     1. Novi projekt java with maven
     2. Versioning -> Initialize git repository 
     3. Git repozitorij na netu (Niti ignore niti read me), sve mora biti prazno
-    4 Git -> remote -> push
+    4. Git commit prvo
+    5. Git -> remote -> push
+    
+    
+    
     */
 }
